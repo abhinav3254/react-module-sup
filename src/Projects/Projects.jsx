355ace353c50fdaa1dc8e08b0118be66e71756a1
abhinav3254 <abhinav3254@gmail.com>
2024-02-03 00:21:34 +0530

    card added for projects

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './Projects.scss'
+import React from 'react';
+import './Projects.scss';
+import javaImage from '../images/skills/java.svg';
 
 function Projects() {
     return (
-        <div>Projects</div>
-    )
+        <div className="card">
+            <img className="img" src={javaImage} alt="Java Image" />
+            <div className="textBox">
+                <p className="text head">Ethereum</p>
+                <span>Cryptocurrency</span>
+                <p className="text price">1.654,34€</p>
+            </div>
+        </div>
+    );
 }
 
-export default Projects
+export default Projects;

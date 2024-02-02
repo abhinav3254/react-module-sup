dbc5b77dbe5a0e5988aa455d69bff428e035dca9
abhinav3254 <abhinav3254@gmail.com>
2024-02-02 15:31:45 +0530

    spinning effect

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import './Home.scss';
 
 function Home() {
-
     return (
         <div className='home-main' >
             <div className="home-content">
                 <p className='greet'>Hello EveryOne</p>
                 <p className='name'>I'm <span>Abhinav Kumar</span></p>
                 <p className='des'>SOFTWARE ENGINEER</p>
-                <p className='exp'>MEAN <span>|</span> MERN <span>|</span> MOBILE <span>|</span> BACKEND</p>
+                <p id='spin' className='exp'></p>
             </div>
         </div>
     );

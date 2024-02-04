07ccd84f84bb520acc44ed04619d066f093675e7
abhinav3254 <abhinav3254@gmail.com>
2024-02-04 20:21:51 +0530

    projects section done

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import './Projects.scss';
-import javaImage from '../images/skills/java.svg';
+import projectsJson from '../json/projects/projects.json'
 
 function Projects() {
     return (
-        <div class="card">
-            <img src="https://codingyaar.com/wp-content/uploads/card-image.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
+        // <div className="projects">
+
+        //     <div class="card">
+        //         <img src="https://codingyaar.com/wp-content/uploads/card-image.jpg" class="card-img-top" alt="..." />
+        //         <div class="card-body">
+        //             <h5 class="card-title">Card title</h5>
+        //             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        //             <button>Go somewhere</button>
+        //         </div>
+        //     </div>
+
+        // </div>
+
+        <div className="projects">
+            {projectsJson.map((projectsJson, index) => (
+                <div key={index} className="card">
+                    <img src={projectsJson.image} className="card-img-top" alt={projectsJson.title} />
+                    <div className="card-body">
+                        <h5 className="card-title">{projectsJson.title}</h5>
+                        <p className="card-text">{projectsJson.desc}</p>
+                        <p className="card-text">Technologies: {projectsJson.technology.join(', ')}</p>
+                        <button className='button'>github</button>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }

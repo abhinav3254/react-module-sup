a35bdda03787e5ba7ea27ffe8950d94edef1118e
abhinav3254 <abhinav3254@gmail.com>
2024-02-18 21:25:11 +0530

    fixed scss

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import './Nav.scss';
 import { Link } from 'react-router-dom';
+import logo from '../images/logo.svg';
 
 function Nav() {
     return (
         <div className='nav-main'>
             <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">Navbar</Link>
+                    {/* Logo on the left */}
+                    <Link className="navbar-brand" to="/">
+                        <img src={logo} alt="" />
+                    </Link>
+                    {/* Hamburger menu button */}
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    {/* Navigation links aligned to the right */}
+                    <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link active" to="/">Home</Link>
+                                <Link className="nav-link" to="/">HOME</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/about">About</Link>
+                                <Link className="nav-link" to="/about">ABOUT</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/skills">Skills</Link>
+                                <Link className="nav-link" to="/skills">SKILLS</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/projects">Projects</Link>
+                                <Link className="nav-link" to="/projects">PROJECTS</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/contact">Contact</Link>
+                                <Link className="nav-link" to="/contact">CONTACT</Link>
                             </li>
                         </ul>
                     </div>
